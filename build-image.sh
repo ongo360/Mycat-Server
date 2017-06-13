8b7381513ccd9140aa16cd8130712dcb18dce3fc
jackychenb <jackychenb@jackychbtekiMBP.lan>
2017-06-13 20:13:04 +0800

    Adjust Image Builder to support SQLServer

--- build-image.sh
+++ build-image.sh
@@ -3,4 +3,10 @@
 
 mvn clean package -Dmaven.test.skip=true
 
-docker build -t ongo360/mycat:1.6.1  .
+docker build -t ongo360/mycat:1.6.1 .
+
+docker tag ongo360/mycat:1.6.1 registry.cn-hangzhou.aliyuncs.com/ongo360/mycat:1.6.1 
+
+docker login --username=at1927u0d@aliyun.com registry.cn-hangzhou.aliyuncs.com
+
+docker push registry.cn-hangzhou.aliyuncs.com/ongo360/mycat:1.6.1
